c03a862b14605d135f8c8b82ec93722f416d3819
Matthieu Moquet <matthieu@moquet.net>
2013-06-22 19:00:42 +0200

    Initial commit

---
+++ src/Ketmo/Command/AcmeCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ketmo\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AcmeCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('acme')
+            ->setDescription('Hack me')
+            ->addArgument('name', InputArgument::OPTIONAL)
+            ->addOption('yell', null, InputOption::VALUE_NONE)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $text = '<info>Hello</info>';
+
+        $name = $input->getArgument('name');
+        if ($name) {
+            $text .= ' <comment>'.$name.'</comment>';
+        }
+
+        if ($input->getOption('yell')) {
+            $text .= ' !!!';
+        }
+
+        $output->writeln($text);
+    }
+}
