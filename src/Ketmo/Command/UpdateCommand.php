09cfc4bbeb81ea0cb9c8022f57e78a7233e27ddf
Matthieu Moquet <matthieu@moquet.net>
2013-06-22 19:38:51 +0200

    Add update command

---
+++ src/Ketmo/Command/UpdateCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Ketmo\Command;
+
+use Herrera\Phar\Update\Manager;
+use Herrera\Phar\Update\Manifest;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UpdateCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('update')
+            ->setDescription('Self update PHAR file')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $manager = new Manager(Manifest::loadFile(
+            'http://mattketmo.github.io/my-cli/manifest.json'
+        ));
+
+        $output->writeln('Looking for updates...');
+
+        if ($manager->update('1.0.0', true)) {
+            $output->writeln('<info>Updated to latest version</info>');
+        } else {
+            $output->writeln('<comment>Already up-to-date.</comment>');
+
+        }
+    }
+}
